a0f90bcf968435a3e73b9c49b9ea8bef01211b80
Fatmakrcyrr <148495037+Fatmakrcyrr@users.noreply.github.com>
2023-10-22 00:14:52 +0300

    sql ödev3
    sqllesson3

---
+++ sqllesson3.sql
@@ -0,0 +1,4 @@
+select country from country where country like 'A%a' ; ---soru1
+select country from country where  country like'_____%n'; ---soru2
+select title from film where title ilike'%t%t%t%'; ---soru3
+select * from film where title like'T%' and length>90 and rental_rate=2.99; ----soru4
